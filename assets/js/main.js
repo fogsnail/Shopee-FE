5a0ab453270ab7a4115819d26b2d2c949a30528e
Huy Hoang <huyhoangnd237@gmail.com>
2020-06-18 19:36:12 +0700

    add full

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// Login Form 
+var modal = document.getElementById('modal');
+
+var loginForm = document.getElementById('loginForm');
+var openLoginForm = document.getElementById('openLoginForm');
+var closeLoginForm = document.getElementById('closeLoginForm')
+
+openLoginForm.addEventListener('click',function() {
+    modal.style.display='flex';
+})
+
+closeLoginForm.addEventListener('click',function() {
+    modal.style.display='none';
+})
+
+
+
+
+
+
+
+
+// Register Form
+var registerForm = document.getElementById('registerForm');
+var openRegisterForm = document.getElementById('openRegisterForm');
+
